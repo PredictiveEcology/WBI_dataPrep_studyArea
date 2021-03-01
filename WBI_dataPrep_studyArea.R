3dcd277049c486172446976ad30109cbb346c979
Alex Chubaty <achubaty@for-cast.ca>
2021-03-01 10:09:19 -0700

    upadet species used: Abie_las and ABie_bal

--- WBI_dataPrep_studyArea.R
+++ WBI_dataPrep_studyArea.R
@@ -157,19 +157,19 @@ Init <- function(sim) {
 
   ## all species considered in western boreal (will be subset later)
   data("sppEquivalencies_CA", package = "LandR", envir = environment())
-  allWBIspp <- c("Abie_Las", "Betu_Pap", "Lari_Lar",
+  allWBIspp <- c("Abie_Bal", "Abie_Las", "Betu_Pap", "Lari_Lar",
                  "Pice_Eng", "Pice_Gla", "Pice_Mar",
                  "Pinu_Ban", "Pinu_Con", "Popu_Tre")
   sppEquiv <- sppEquivalencies_CA[KNN %in% allWBIspp]
   wbiSppToUse <- data.table(
     LandR = sppEquiv[, LandR],
-    BC = c(TRUE,  TRUE, TRUE, TRUE,  TRUE, TRUE, FALSE, TRUE,  TRUE),
-    AB = c(TRUE,  TRUE, TRUE, FALSE, TRUE, TRUE, TRUE,  TRUE,  TRUE), # Pice_eng?
-    SK = c(TRUE,  TRUE, TRUE, FALSE, TRUE, TRUE, TRUE,  FALSE, TRUE),
-    MB = c(TRUE,  TRUE, TRUE, FALSE, TRUE, TRUE, TRUE,  FALSE, TRUE),
-    YT = c(TRUE,  TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, TRUE),
-    NT = c(FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE,  FALSE, TRUE), ## run with NU, so needs to be same
-    NU = c(FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE,  FALSE, TRUE)  ## run with NT, so needs to be same
+    BC = c(FALSE, TRUE,  TRUE, TRUE, TRUE,  TRUE, TRUE, FALSE, TRUE,  TRUE),
+    AB = c(TRUE,  TRUE,  TRUE, TRUE, FALSE, TRUE, TRUE, TRUE,  TRUE,  TRUE), # Pice_eng?
+    SK = c(TRUE,  FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE,  FALSE, TRUE),
+    MB = c(TRUE,  FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE,  FALSE, TRUE),
+    YT = c(FALSE, TRUE,  TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE,  TRUE),
+    NT = c(FALSE, FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE,  FALSE, TRUE), ## run with NU, so needs to be same
+    NU = c(FALSE, FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE,  FALSE, TRUE)  ## run with NT, so needs to be same
   )
   sAN <- if (studyAreaName == "RIA") "BC" else studyAreaName
   sim$sppEquiv <- sppEquiv[which(wbiSppToUse[, ..sAN][[1]]), ] ## subset per study area
